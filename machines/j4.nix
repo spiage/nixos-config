4770d8ca2a8e1e6b76872b408905938f3b66b9ef
spiage <spiage@mail.ru>
2024-06-18 06:43:50 +0300

    j4 vpool added

--- machines/j4.nix
+++ machines/j4.nix
@@ -109,6 +109,7 @@
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
     /srv/nfs         192.168.0.0/16(rw,fsid=0,no_subtree_check)
+    /srv/nfs/vpool   192.168.0.0/16(rw,nohide,insecure,no_subtree_check)
   '';
     # /export         192.168.1.10(rw,fsid=0,no_subtree_check) 192.168.1.15(rw,fsid=0,no_subtree_check)
     # /export/kotomi  192.168.1.10(rw,nohide,insecure,no_subtree_check) 192.168.1.15(rw,nohide,insecure,no_subtree_check)
