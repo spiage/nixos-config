e7010e2d62b8ce035199fc7cc4d888183df9f7ca
spiage <spiage@mail.ru>
2024-11-09 23:29:27 +0300

    Change samba settings to new schema

--- machines/j4.nix
+++ machines/j4.nix
@@ -48,20 +48,20 @@
     enable = true;
     securityType = "user";
     openFirewall = true;
-    extraConfig = ''
-      workgroup = WORKGROUP
-      server string = smbnix
-      netbios name = smbnix
-      security = user 
-      #use sendfile = yes
-      #max protocol = smb2
-      # note: localhost is the ipv6 localhost ::1
-      hosts allow = 192.168.1. 127.0.0.1 localhost
-      hosts deny = 0.0.0.0/0
-      guest account = nobody
-      map to guest = bad user
-    '';
-    shares = {
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "smbnix";
+        "netbios name" = "smbnix";
+        "security" = "user";
+        #"use sendfile" = "yes";
+        #"max protocol" = "smb2";
+        # note: localhost is the ipv6 localhost ::1
+        "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+      };    
       public = {
         path = "/srv/Shares/Public";
         browseable = "yes";
@@ -97,7 +97,6 @@
         "force user" = "spiage";
         #"force group" = "groupname";
       };
-
     };
   };
 
