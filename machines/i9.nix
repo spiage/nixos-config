ca5c53fe9ac4aa42987741d10fe989b52a642b97
spiage <spiage@mail.ru>
2025-04-29 22:11:13 +0300

    Add acceleration

--- machines/i9.nix
+++ machines/i9.nix
@@ -139,6 +139,7 @@
   services.ollama = {
     enable = true;
     package = (pkgs.ollama.override { acceleration = "cuda"; });
+    acceleration = "cuda";
     environmentVariables = {
       #     HIP_VISIBLE_DEVICES = "0,1";
       #     OLLAMA_LLM_LIBRARY = "cpu";
