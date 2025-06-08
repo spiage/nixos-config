c143166df0b51a9465b4f13d28972b81b93a2e26
spiage <spiage@mail.ru>
2025-06-08 21:59:45 +0300

    Temporary remove smb from j4

--- machines/j4.nix
+++ machines/j4.nix
@@ -68,91 +68,91 @@
   #     options = [ "subvol=Shares/Private" "noatime" ]; # "compress=zstd" "noatime" ];
   #   };
 
-  fileSystems."/srv" =
-    { device = "/dev/disk/by-uuid/8f0042c2-b6ac-4d9a-b08a-9b5b7a6c809c";
-      fsType = "xfs";
-      options = [ "noatime" ]; # "compress=zstd" "noatime" ];
-    };
+  # fileSystems."/srv" =
+  #   { device = "/dev/disk/by-uuid/8f0042c2-b6ac-4d9a-b08a-9b5b7a6c809c";
+  #     fsType = "xfs";
+  #     options = [ "noatime" ]; # "compress=zstd" "noatime" ];
+  #   };
 
-  services.samba = {
-    enable = true;
-    openFirewall = true;
-    settings = {
-      global = {
-        "workgroup" = "WORKGROUP";
-        "server string" = "j4";
-        "netbios name" = "j4";
-        "security" = "user";
-        #"use sendfile" = "yes";
-        #"max protocol" = "smb2";
-        # note: localhost is the ipv6 localhost ::1
-        "hosts allow" = "192.168.1. 127.0.0.1 localhost";
-        "hosts deny" = "0.0.0.0/0";
-        "guest account" = "nobody";
-        "map to guest" = "bad user";
-      };    
-      public = {
-        path = "/srv/Shares/Public";
-        browseable = "yes";
-        writable = "yes";
-        public = "yes";
-        "read only" = "no";
-        "guest ok" = "yes";
-        "create mask" = "0644";
-        "directory mask" = "0755";
-        "force user" = "spiage";
-        #"force group" = "groupname";
-      };
-      private = {
-        path = "/srv/Shares/Private";
-        browseable = "yes";
-        writable = "yes";
-        "read only" = "no";
-        "guest ok" = "no";
-        "create mask" = "0744";
-        "directory mask" = "0755";
-        "force user" = "spiage";
-        #"force group" = "groupname";
-      };
-      public1 = {
-        path = "/home/spiage/p1";
-        browseable = "yes";
-        writable = "yes";
-        public = "yes";
-        "read only" = "no";
-        "guest ok" = "yes";
-        "create mask" = "0644";
-        "directory mask" = "0755";
-        "force user" = "spiage";
-        #"force group" = "groupname";
-      };
-    };
-  };
+  # services.samba = {
+  #   enable = true;
+  #   openFirewall = true;
+  #   settings = {
+  #     global = {
+  #       "workgroup" = "WORKGROUP";
+  #       "server string" = "j4";
+  #       "netbios name" = "j4";
+  #       "security" = "user";
+  #       #"use sendfile" = "yes";
+  #       #"max protocol" = "smb2";
+  #       # note: localhost is the ipv6 localhost ::1
+  #       "hosts allow" = "192.168.1. 127.0.0.1 localhost";
+  #       "hosts deny" = "0.0.0.0/0";
+  #       "guest account" = "nobody";
+  #       "map to guest" = "bad user";
+  #     };    
+  #     public = {
+  #       path = "/srv/Shares/Public";
+  #       browseable = "yes";
+  #       writable = "yes";
+  #       public = "yes";
+  #       "read only" = "no";
+  #       "guest ok" = "yes";
+  #       "create mask" = "0644";
+  #       "directory mask" = "0755";
+  #       "force user" = "spiage";
+  #       #"force group" = "groupname";
+  #     };
+  #     private = {
+  #       path = "/srv/Shares/Private";
+  #       browseable = "yes";
+  #       writable = "yes";
+  #       "read only" = "no";
+  #       "guest ok" = "no";
+  #       "create mask" = "0744";
+  #       "directory mask" = "0755";
+  #       "force user" = "spiage";
+  #       #"force group" = "groupname";
+  #     };
+  #     public1 = {
+  #       path = "/home/spiage/p1";
+  #       browseable = "yes";
+  #       writable = "yes";
+  #       public = "yes";
+  #       "read only" = "no";
+  #       "guest ok" = "yes";
+  #       "create mask" = "0644";
+  #       "directory mask" = "0755";
+  #       "force user" = "spiage";
+  #       #"force group" = "groupname";
+  #     };
+  #   };
+  # };
 
-  services.samba-wsdd = {
-    enable = true;
-    openFirewall = true;
-  };
+  # services.samba-wsdd = {
+  #   enable = true;
+  #   openFirewall = true;
+  # };
 
-  services.avahi = {
-    publish.enable = true;
-    publish.userServices = true;
-    # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
-    nssmdns4 = true;
-    # ^^ Not one hundred percent sure if this is needed- if it aint broke, don't fix it
-    enable = true;
-    openFirewall = true;
-  };
+  # services.avahi = {
+  #   publish.enable = true;
+  #   publish.userServices = true;
+  #   # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
+  #   nssmdns4 = true;
+  #   # ^^ Not one hundred percent sure if this is needed- if it aint broke, don't fix it
+  #   enable = true;
+  #   openFirewall = true;
+  # };
 
-  # networking.firewall.enable = true;
-  networking.firewall.allowPing = true;
-  networking.firewall.extraCommands = ''iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns'';
-  networking.firewall.allowedTCPPorts = [ 7946 ];
+  # # networking.firewall.enable = true;
+  # networking.firewall.allowPing = true;
+  # networking.firewall.extraCommands = ''iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns'';
+  # networking.firewall.allowedTCPPorts = [ 7946 ];
   
-  services.nfs.server.enable = true;
-  services.nfs.server.exports = ''
-    /srv/nfs         192.168.0.0/16(rw,fsid=0,no_subtree_check,no_root_squash,sync)
-    /srv/nfs/vpool   192.168.0.0/16(rw,nohide,insecure,no_subtree_check,no_root_squash,sync)
-  '';
+  # services.nfs.server.enable = true;
+  # services.nfs.server.exports = ''
+  #   /srv/nfs         192.168.0.0/16(rw,fsid=0,no_subtree_check,no_root_squash,sync)
+  #   /srv/nfs/vpool   192.168.0.0/16(rw,nohide,insecure,no_subtree_check,no_root_squash,sync)
+  # '';
 
 }
